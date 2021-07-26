36f208caf91077b7196ec64cf697d1f5b4e3503e
Eug1ena <kjuner8@gmail.com>
2021-07-26 20:15:31 +0900

    BinaryIndexedTreeの型を一般化した

--- BinaryIndexedTree.cpp
+++ BinaryIndexedTree.cpp
@@ -1,18 +1,19 @@
+template<class T>
 struct BinaryIndexedTree{
     int sz;
-    vector<int> data;
+    vector<T> data;
     
-    BinaryIndexedTree(int n): sz(n), data(n - 1, 0) {}
+    BinaryIndexedTree(int n): sz(n), data(n + 1, 0) {}
     
-    void update(int k, int x){
+    void add(int k, T x){
         k++;
         for(int i = k; i <= sz; i += (i & -i)){
             data[i] += x;
         }
     }
     
-    int query(int r){ // [0, r)
-        int ret = 0;
+    T query(int r){ // [0, r)
+        T ret = 0;
         for(int i = r; i >= 1; i -= (i & -i)){
             ret += data[i];
         }
