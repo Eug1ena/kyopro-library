81cd102ecf201e0381278cc568b56ea37ad38195
Eug1ena <kjuner8@gmail.com>
2021-07-26 20:40:53 +0900

    SegmentTree.cppを変え忘れたので一般化したものを入れた

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,32 +1,42 @@
+template<class T>
 struct SegmentTree{
-    int sz;
-    vector<int> data;
+    using F = function<T(T, T)>;
     
-    SegmentTree(int n){
+    int sz;
+    vector<T> data;
+    
+    F f;
+    T id;
+    
+    SegmentTree(int n, F f, T id): f(f), id(id) {
         sz = 1;
         while(sz < n) sz *= 2;
-        data.resize(sz * 2, 1e9);
+        data.resize(sz * 2, id);
     }
     
-    void update(int k, int x){
+    void update(int k, T x){
         k += sz;
         data[k] = x;
         while(k > 1){
             k /= 2;
-            data[k] = min(data[k * 2], data[k * 2 + 1]);
+            data[k] = f(data[k * 2], data[k * 2 + 1]);
         }
     }
     
-    int query(int a, int b, int l, int r, int at){
+    T query(int a, int b, int l, int r, int at){
         if(a <= l && r <= b){
             return data[at];
         }else if(r <= a || b <= l){
-            return 1e9;
+            return id;
         }
-        return min(query(a, b, l, (l + r) / 2, at * 2), query(a, b, (l + r) / 2, r, at * 2 + 1));
+        return f(query(a, b, l, (l + r) / 2, at * 2), query(a, b, (l + r) / 2, r, at * 2 + 1));
     }
     
-    int query(int a, int b){
+    T query(int a, int b){
         return query(a, b, 0, sz, 1);
+    }
+    
+    T operator [](int k){
+        return data[k + sz];
     }
 };
