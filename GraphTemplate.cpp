1e893bb46880829a481a05a9183f3301934e6760
Eug1ena <kjuner8@gmail.com>
2021-08-14 16:31:39 +0900

    GraphTemplate.cppをより便利に

--- GraphTemplate.cpp
+++ GraphTemplate.cpp
@@ -2,28 +2,37 @@ template<class T = int>
 struct Edge{
     int from, to;
     T cost;
-
+    int id;
+ 
     Edge() = default;
-    Edge(int from, int to, T cost = 1) : from(from), to(to), cost(cost) {}
-
+    Edge(int from, int to, T cost = 1, int id = -1) : from(from), to(to), cost(cost), id(id) {}
+ 
     operator int() const { return to; }
 };
 
 template<class T = int>
-struct Graph {
+struct Graph{
+private:
     vector<vector<Edge<T>>> g;
+public:
+    int edge_cnt = 0;
     T INF = numeric_limits<T>::max() / 4;
-
+ 
     Graph() = default;
-
+ 
     explicit Graph(int n) : g(n) {}
     
     size_t size(){
         return g.size();
     }
     
+    void add_directed_edge(int from, int to, T cost = 1){
+        g[from].push_back({from, to, cost, edge_cnt++});
+    }
+    
     void add_edge(int from, int to, T cost = 1){
-        g[from].push_back({from, to, cost});
+        g[from].push_back({from, to, cost, edge_cnt});
+        g[to].push_back({to, from, cost, edge_cnt++});
     }
     
     vector<Edge<T>>& operator [](int k){
