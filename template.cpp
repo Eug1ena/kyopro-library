2876fa3708b10ca937817bf332e9c1d497a5858a
Eug1ena <kjuner8@gmail.com>
2021-08-18 19:35:07 +0900

    template.cppのnext_sequence, rnd, position, inv2を削除

--- template.cpp
+++ template.cpp
@@ -15,20 +15,15 @@ constexpr lint mod = 998244353;
 constexpr int inf9 = 1e9; constexpr lint inf18 = 1e18;
 inline void Yes(bool condition){ if(condition) cout << "Yes" << endl; else cout << "No" << endl; }
 lint power(lint base, lint exponent, lint module){ if(exponent % 2){ return power(base, exponent - 1, module) * base % module; }else if(exponent){ lint root_ans = power(base, exponent / 2, module); return root_ans * root_ans % module; }else{ return 1; }}
-struct position{ int x, y; }; position mv[4] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}}; double euclidean(position first, position second){ return sqrt((second.x - first.x) * (second.x - first.x) + (second.y - first.y) * (second.y - first.y)); }
 template<class itr> void array_output(itr start, itr goal){ for(auto i = start; i != goal; i++) cout << (i == start ? "" : " ") << (*i); cout << endl; }
 template<class itr> void cins(itr first, itr last){ for(auto i = first; i != last; i++){ cin >> (*i); } }
 template<class T> T gcd(T a, T b){ if(b) return gcd(b, a % b); else return a; }
 template<class T> T lcm(T a, T b){ return a / gcd(a, b) * b; }
 namespace comb{ vector<lint> fact, factinv; void comb_init(int sz){ fact.resize(sz + 1); factinv.resize(sz + 1); fact[0] = 1; for(int i = 1; i <= sz; i++){ fact[i] = fact[i - 1] * i % mod; } factinv[sz] = power(fact[sz], mod - 2, mod); for(int i = sz - 1; i >= 0; i--){ factinv[i] = factinv[i + 1] * (i + 1) % mod; } } lint P(int n, int r){ if(r < 0 || n < r) return 0; return (fact[n] * factinv[n - r] % mod); } lint C(int p, int q){ if(q < 0 || p < q) return 0; return (fact[p] * factinv[q] % mod * factinv[p - q] % mod); } };
-template<class itr> bool next_sequence(itr first, itr last, int max_bound){ itr now = last; while(now != first){ now--; (*now)++; if((*now) == max_bound){ (*now) = 0; }else{ return true; } } return false; }
-template<class itr, class itr2> bool next_sequence2(itr first, itr last, itr2 first2, itr2 last2){ itr now = last; itr2 now2 = last2; while(now != first){ now--, now2--; (*now)++; if((*now) == (*now2)){ (*now) = 0; }else{ return true; } } return false; }
 template<class T> bool chmax(T &a, const T &b){ if(a < b){ a = b; return 1; } return 0; }
 template<class T> bool chmin(T &a, const T &b){ if(b < a){ a = b; return 1; } return 0; }
 inline int at(lint i, int j){ return (i >> j) & 1; }
-random_device rnd;
 bool is_inside(lint y, lint x, lint H, lint W){ return (0 <= y && y < H && 0 <= x && x < W); }
-lint inv2 = power(2, mod - 2, mod);
 
 struct io_init {
    io_init() {
